1e629070c1b531f796398f2676e687fb79d7e764
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-31 14:17:22 +0530

    update navigation

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,18 @@
-
+import {Link, NavLink} from 'react-router-dom';
+import Logo from '../logo.svg';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='header'>
+      <Link className='logo' to='/'>
+        <img src={Logo} alt="logo" />
+        <span>ROuter Navigation</span>
+      </Link>
+      <nav>
+        <NavLink to='/'>Home</ NavLink>
+        <NavLink to='/products'>Products</ NavLink>
+        <NavLink to='/contact'>Contact</ NavLink>
+      </nav>
+    </div>
   )
 }
