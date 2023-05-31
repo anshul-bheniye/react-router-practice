deeeb67607516a255bc6a132761096d1d823a091
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-31 14:46:10 +0530

    add github link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@
 export const Footer = () => {
   return (
     <div className="footer">
-        2050 - Route Navigation
+       <div> 2050 - Route Navigation - Made by <span><a href="https://github.com/anshul-bheniye">Human Being</a></span></div>
     </div>
   )
 }
