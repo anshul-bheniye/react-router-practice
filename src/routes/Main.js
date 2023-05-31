0168408bfef895c03e4529bc03e83cbe1125704e
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-31 13:45:22 +0530

    routes navigate

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,4 +1,4 @@
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Navigate} from 'react-router-dom';
 import {Home} from '../components/Home';
 import {Product} from '../components/Product';
 import {Admin} from '../components/Admin';
@@ -8,12 +8,14 @@ import {Contact} from '../components/Contact';
 
  
 export const Main = () => {
+
+    const user = false;
   return (
     <div className='main'>
         <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='products' element={<Product />}></Route>
-            <Route path='admin' element={<Admin />}></Route>
+            <Route path='/admin' element={user ? <Admin /> : <Navigate to='pagenotfound'/>}></Route>
             <Route path='contact' element={<Contact />}></Route>
             <Route path='productList' element={<ProductList />}></Route>
             <Route path='pagenotfound' element={<PageNotFound/>} />
