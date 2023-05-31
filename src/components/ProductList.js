746b9d94242df6a707b15f57737193deb8c39930
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-31 15:57:26 +0530

    use params

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,7 +1,15 @@
+import { useParams } from "react-router-dom"
+
  
 
 export const ProductList = () => {
+  const params = useParams();
+  console.log(params)
+
   return (
-    <div>ProductList</div>
+    <div>
+      <h3>Product List</h3>
+      <p>Product number {params.id}</p>
+    </div>
   )
 }
