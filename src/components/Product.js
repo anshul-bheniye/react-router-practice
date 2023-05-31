8dfc22ce6e5430cdbaa94b9ba968548569b60799
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-31 14:17:05 +0530

    update product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,10 +1,18 @@
+import { useNavigate } from "react-router-dom"
+
  
 
 export const Product = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    console.log('--+--');
+    navigate('/productList');
+  }
   return (
     <div>
         <h2>Products</h2>
-        <button>On Product List</button>
+        <button onClick={handleClick}>On Product List</button>
     </div>
   )
 }
