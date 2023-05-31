e5871dbd9c40ba2c84ea6fe260ddb92589bc1f7a
Anshul Bheniye <81631443+anshul-bheniye@users.noreply.github.com>
2023-05-31 14:17:42 +0530

    import component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import {Header} from './components/Header';
+import {Footer} from './components/Footer';
+import {Main} from './routes/Main';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-     
+     <Header />
+     <Main />
+     <Footer />
     </div>
   );
 }
